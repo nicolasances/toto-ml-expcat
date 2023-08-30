80c3a2e0d8ded8b7127dab9f7a0c49bbd75deb6e
nicolasances <nicolas.matteazzi@gmail.com>
2023-08-30 19:55:26 +0200

    Added CORS

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
 from model import expcat
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000", "https://toto-reactjs-expenses-6lv62poq7a-ew.a.run.app"])
 
 @app.route('/predict', methods=['GET'])
 def predict():
