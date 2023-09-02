8080d41f6fa797c582572731d779d90b101d43f0
nicolasances <nicolas.matteazzi@gmail.com>
2023-09-02 20:20:26 +0200

    Added prod domain

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from model import expcat
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "https://toto-reactjs-expenses-6lv62poq7a-ew.a.run.app"])
+CORS(app, origins=["http://localhost:3000", "https://toto-reactjs-expenses-6lv62poq7a-ew.a.run.app", "https://toto-reactjs-expenses-zkz7cuplpa-ew.a.run.app"])
 
 @app.route('/predict', methods=['GET'])
 def predict():
