a4be673f2a3f0e888b685cec8c2418d78337c2ab
nicolasances <nicolas.matteazzi@gmail.com>
2023-09-12 11:24:32 +0200

    Added a smoke API endpoint

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins=["http://localhost:3000", "https://toto-reactjs-expenses-6lv62poq7a-ew.a.run.app", "https://toto-reactjs-expenses-zkz7cuplpa-ew.a.run.app"])
 
+@app.route('/', methods=['GET'])
+def smoke():
+    return {"api": "expcat", "running": True}
+
 @app.route('/predict', methods=['GET'])
 def predict():
     return expcat.predict(request.args.get("description"), request.args.get("email"))
