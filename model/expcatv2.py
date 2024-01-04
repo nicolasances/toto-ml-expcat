8642471b9fc84b8374cde28acb29d69e1b788c53
nicolasances <nicolas.matteazzi@gmail.com>
2024-01-04 15:56:39 +0100

    Start restructuring EXPCAT

---
+++ model/expcatv2.py
@@ -0,0 +1,18 @@
+
+
+class ModelExpcat: 
+
+    def train():
+        """Trains the model on the latest data
+
+        The training process performs the following: 
+        - Download data from Expenses API
+        - Splits the data in training and test
+        - Trains 1 model for each user
+        - Scores the model on both training and test data
+        - Persists the trained model (as the latest version) and metrics 
+        """
+        pass
+
+    
+
